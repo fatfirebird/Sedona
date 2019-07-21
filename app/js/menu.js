d08cfdab9b4973f6b3412e12521181bba07b137c
Pavel Konovalov <fatfirebird@gmail.com>
2019-07-21 17:19:57 +0500

    fix mobile menu

--- app/js/menu.js
+++ app/js/menu.js
@@ -1,22 +1,23 @@
 let menuShow = document.querySelector('.button--menu');
 let menuClose = document.querySelector('.button--close')
 
-menuShow.addEventListener('click', menu);
-menuClose.addEventListener('click', menu );
+menuShow.addEventListener('click', showMenu);
+menuClose.addEventListener('click', showMenu);
 
-function menu(evt) {
-  evt.preventDefault();
+function showMenu() {
   navigation = document.getElementsByClassName('nav-item');
-  if (navigation[0].style.display == '' || navigation[0].style.display == 'none') {
+  if (navigation[0].style.display == 'none') {
     for (let i = 0; i < navigation.length; i++) {
       navigation[i].style.display = 'block';
     }
   } else
-  if (navigation[0].style.display == 'block') {
+  if (navigation[0].style.display == '' || navigation[0].style.display == 'block') {
     for (let i = 0; i < navigation.length; i++) {
       navigation[i].style.display = 'none';
     }
   }
   logo = document.querySelector('.nav-item--logo');
   logo.style.display = 'block';
-}
+};
+
+showMenu();
